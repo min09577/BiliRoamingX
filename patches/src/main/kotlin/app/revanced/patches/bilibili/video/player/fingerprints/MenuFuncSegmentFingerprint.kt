2fa9b05aefd34261b06a60e8ed0c2b595092e310
min09577 <min09577@users.noreply.github.com>
2026-05-28 00:29:18 +0800

    fix: two more fingerprints for 8.95.0
    - MenuServiceCreateSpeedFingerprint: inner class  ->
    - MenuFuncSegmentFingerprint: option-more -> full-more
    - These fix RememberPlaybackSpeed patch partially

--- patches/src/main/kotlin/app/revanced/patches/bilibili/video/player/fingerprints/MenuFuncSegmentFingerprint.kt
+++ patches/src/main/kotlin/app/revanced/patches/bilibili/video/player/fingerprints/MenuFuncSegmentFingerprint.kt
@@ -3,7 +3,7 @@ package app.revanced.patches.bilibili.video.player.fingerprints
 import app.revanced.patcher.fingerprint.MethodFingerprint
 
 object MenuFuncSegmentFingerprint : MethodFingerprint(
-    strings = listOf("player.player.option-more.speed.player"),
+    strings = listOf("player.player.full-more.speed.player"),
     parameters = listOf("F"),
     returnType = "V",
 )
--- patches/src/main/kotlin/app/revanced/patches/bilibili/video/player/fingerprints/MenuServiceCreateSpeedFingerprint.kt
+++ patches/src/main/kotlin/app/revanced/patches/bilibili/video/player/fingerprints/MenuServiceCreateSpeedFingerprint.kt
@@ -3,7 +3,7 @@ package app.revanced.patches.bilibili.video.player.fingerprints
 import app.revanced.patcher.fingerprint.MethodFingerprint
 
 object MenuServiceCreateSpeedFingerprint : MethodFingerprint(
-    strings = listOf("MenuService\$createSpeed$1", "theseus-united"),
+    strings = listOf("MenuService\$createSpeed$2", "theseus-united"),
     parameters = listOf("I"),
     returnType = "V",
 )
