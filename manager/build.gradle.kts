b23c2be60fd07e46cb16344d6216114ff371ebfe
min <397824888@qq.com>
2026-06-15 16:22:02 +0800

    fix: 移除BC排除规则, bcpkix需要bcprov/bcutil

--- manager/build.gradle.kts
+++ manager/build.gradle.kts
@@ -37,11 +37,7 @@ dependencies {
     }
 
     // BouncyCastle PKIX for APK signing
-    // Android 只内置 bcprov, 不含 bcpkix (CMS/证书生成)
-    implementation("org.bouncycastle:bcpkix-jdk18on:1.78") {
-        exclude(group = "org.bouncycastle", module = "bcprov-jdk18on")
-        exclude(group = "org.bouncycastle", module = "bcutil-jdk18on")
-    }
+    implementation("org.bouncycastle:bcpkix-jdk18on:1.78")
 
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
